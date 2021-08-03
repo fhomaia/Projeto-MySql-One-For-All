bd9ad1497bc861a46367e3d4ad8d9cf1515c18c1
Fernando <fhomaia@gmail.com>
2021-08-03 20:18:40 -0300

    fix: exercicio 3

--- desafio3.sql
+++ desafio3.sql
@@ -2,4 +2,4 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.name AS usuario, s.title AS nome FROM SpotifyClone.history_of_replays hr
 LEFT JOIN SpotifyClone.users u ON hr.user_id = u.user_id
 LEFT JOIN SpotifyClone.songs s ON hr.song_id = s.song_id
-ORDER BY usuario;
+ORDER BY usuario, nome;
