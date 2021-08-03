aed0c47cdfb21aa4414357318a078ee30f215c47
Fernando <fhomaia@gmail.com>
2021-08-03 19:41:52 -0300

    fix: code Climate

--- desafio2.sql
+++ desafio2.sql
@@ -2,4 +2,4 @@ CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(song_id) AS cancoes, 
 (SELECT COUNT(*) FROM SpotifyClone.artists) AS artistas,
 COUNT(DISTINCT album_id) AS albuns
-FROM SpotifyClone.songs; 
+FROM SpotifyClone.songs;
