2c67652db90f64beb8ad3e2be75a9b65d532651c
Fernando <fhomaia@gmail.com>
2021-08-03 19:39:13 -0300

    feat: Req 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT COUNT(song_id) AS cancoes, 
+(SELECT COUNT(*) FROM SpotifyClone.artists) AS artistas,
+COUNT(DISTINCT album_id) AS albuns
+FROM SpotifyClone.songs; 
